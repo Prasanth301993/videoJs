0be6efded97f9ff214905ea149fcc499f2ae7230
Prasanth Raji <praji@inniveinc.com>
2020-10-08 01:15:31 +0530

    seek button added in control bar

--- src/app.js
+++ src/app.js
@@ -5,12 +5,15 @@ import Todo from './components/todo';
 import 'video-react/dist/video-react.css';
 import Share from './components/share';
 import Completed from './components/completed';
+import ForwardReplayControl from "./components/forwardReverse";
 
 export default props => {
     return (
         <Player>
             <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
             <ControlBar autoHide={false}>
+                <ForwardReplayControl  order={3.1} mode={"backward"}/>
+                <ForwardReplayControl  order={3.2} mode={"forward"} />
                 <Share order={7} />
                 <Completed order={8} />
                 <FavButton order={9} />
